3187e8126f0dc576ff6a4a4eed3fd129958f7b0c
jkmrto <juanka1331@gmail.com>
2021-01-31 20:08:34 +0100

    first commit

---
+++ lib/phoenix_blog/application.ex
@@ -0,0 +1,32 @@
+defmodule PhoenixBlog.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      PhoenixBlogWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: PhoenixBlog.PubSub},
+      # Start the Endpoint (http/https)
+      PhoenixBlogWeb.Endpoint
+      # Start a worker by calling: PhoenixBlog.Worker.start_link(arg)
+      # {PhoenixBlog.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: PhoenixBlog.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    PhoenixBlogWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end

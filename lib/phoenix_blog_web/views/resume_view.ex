84347d9578fd49b6b185de6ef456ea4da28480a9
jkmrto <juanka1331@gmail.com>
2022-01-23 11:43:52 +0100

    add more information on resume blocks

--- lib/phoenix_blog_web/views/resume_view.ex
+++ lib/phoenix_blog_web/views/resume_view.ex
@@ -3,4 +3,26 @@ defmodule PhoenixBlogWeb.ResumeView do
     namespace: PhoenixBlogWeb.Resume,
     root: "lib/phoenix_blog_web/templates/resume",
     pattern: "**/*"
+
+  import Phoenix.HTML
+
+  def collapse_information_component(id, desc_paragraphs) do
+    ~E"""
+    <div class="more-information-container">
+    <div
+      class="more-information-link collapsed px-4"
+      data-toggle="collapse"
+      data-target="<%= "#" <> "#{id}" %>"
+      aria-expanded="false"
+    >
+      <p style="font-weight: bold">More Information</p>
+      <i class="fa fa-angle-down" aria-hidden="true" style="font-size: 2.5rem"></i>
+    </div>
+
+    <div id="<%= "#{id}" %>" class="mx-3 collapse"> 
+        <%= raw(Enum.map(desc_paragraphs, fn p -> "<p> #{p} </p>" end)) %>
+      </div>
+    </div>
+    """
+  end
 end

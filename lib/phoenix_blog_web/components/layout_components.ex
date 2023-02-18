974f48870f4f03fb5f708e0014a495f29102af6a
jkmrto <juanka1331@gmail.com>
2023-02-18 10:12:40 +0100

    more improvements in the footer

--- lib/phoenix_blog_web/components/layout_components.ex
+++ lib/phoenix_blog_web/components/layout_components.ex
@@ -6,14 +6,18 @@ defmodule PhoenixBlogWeb.Components.LayoutComponents do
     <footer class="flex-column">
       <div id="container" class="d-flex justify-content-around">
         <div class="d-flex flex-column">
-          <a href="https://elixir-lang.org/" target="_blank">Written with Elixir</a>
-          <a href="https://www.phoenixframework.org/" target="_blank">Built with Phoenix</a>
+          <a href="/posts" target="_blank">Posts</a>
+          <a href="/resume" target="_blank">Resume</a>
+          <a href="/contact" target="_blank">Contact</a>
+          <a href="https://www.jkmrto.dev" target="_blank">www.jkmrto.dev @ 2023</a>
         </div>
         <div class="d-flex flex-column">
+          <a href="https://github.com/jkmrto/phoenix_blog" target="_blank">Code at Github</a>
+          <a href="https://elixir-lang.org/" target="_blank">Written with Elixir</a>
+          <a href="https://www.phoenixframework.org/" target="_blank">Built with Phoenix</a>
           <a href="https://github.com/pragdave/earmark" target="_blank">
             Markdown posts rendered with earmark
           </a>
-          <a href="https://github.com/jkmrto/phoenix_blog" target="_blank">Code at Github</a>
         </div>
       </div>
     </footer>

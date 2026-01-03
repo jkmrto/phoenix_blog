0f03699ce290f212158f5b43d8760dd5d935d297
Juan Carlos <jkmrto@gmail.com>
2026-01-03 20:30:17 +0100

    fix config

--- mix.exs
+++ mix.exs
@@ -5,9 +5,9 @@ defmodule PhoenixBlog.MixProject do
     [
       app: :phoenix_blog,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
